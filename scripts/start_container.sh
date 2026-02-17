5a64908b4084144a26545e5fe2168fa0502ecebd
manvendrarocks1998 <manvendrarocks1998@gmail.com>
2026-02-17 14:12:05 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull manvendrarocks1998/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 manvendrarocks1998/simple-python-flask-app:latest
+docker run -d -p 5050:5000 manvendrarocks1998/simple-python-flask-app:latest
