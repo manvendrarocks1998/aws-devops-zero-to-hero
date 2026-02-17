83b3a59f320cb94e2d131022a37cff5d4a5cabe2
manvendrarocks1998 <manvendrarocks1998@gmail.com>
2026-02-17 12:19:20 +0530

    Fix comment formatting in start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pull the Docker image from  Docker Hub
 docker pull manvendrarocks1998/simple-python-flask-app:latest
 
 # Run the Docker image as a container
